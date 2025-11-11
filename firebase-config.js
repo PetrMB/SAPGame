3004fe0c9d8449253fce4b01aaef689e5aaa640f
Petr <38077629+PetrMB@users.noreply.github.com>
2025-11-11 10:14:27 +0100

    Enable Firebase configuration

--- firebase-config.js
+++ firebase-config.js
@@ -15,16 +15,16 @@
 // 5. Zkopíruj své údaje do firebaseConfig níže
 // 6. Změň USE_FIREBASE na true
 
-const USE_FIREBASE = false; // Změň na true pro aktivaci
+const USE_FIREBASE = true; // Změň na true pro aktivaci
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDKLyWM5H-crIoEtnmMNppi4gz6E_O2GKE",
+  authDomain: "sap-gcc-game.firebaseapp.com",
+  databaseURL: "https://sap-gcc-game-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "sap-gcc-game",
+  storageBucket: "sap-gcc-game.firebasestorage.app",
+  messagingSenderId: "209180428899",
+  appId: "1:209180428899:web:0ff7e349b95f75185f0656"
 };
 
 // Inicializace Firebase (pokud je povoleno)
